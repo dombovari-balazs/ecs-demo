e10ba4f0dce2df3982df1df62046167a12659ac4
Dombovári Balázs <45357979+dombovari-balazs@users.noreply.github.com>
2025-09-09 19:33:43 +0000

    feat: add items router

--- src/main.py
+++ src/main.py
@@ -1,8 +1,8 @@
-from typing import Union
-
 from fastapi import FastAPI
+from src.items.router import router as item_router
 
 app = FastAPI()
+app.include_router(item_router)
 
 
 @app.get("/")
@@ -10,6 +10,3 @@ def read_root():
     return {"Hello": "World"}
 
 
-@app.get("/items/{item_id}")
-def read_item(item_id: int, q: Union[str, None] = None):
-    return {"item_id": item_id, "q": q}
